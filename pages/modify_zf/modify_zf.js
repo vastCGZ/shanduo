564ae106eb45495a05279b2422767e8df427dbb5
vastcgz <vastcgz@163.com>
2018-05-31 10:52:00 +0800

    ...

--- pages/modify_zf/modify_zf.js
+++ pages/modify_zf/modify_zf.js
@@ -1,66 +1,41 @@
 // pages/modify_zf/modify_zf.js
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-  
+    isFocus: true,//控制input 聚焦
+    wallets_password_flag: false//密码输入遮罩
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-  
+  set_wallets_password(e) {//获取验证支付密码
+    this.setData({
+      wallets_password: e.detail.value
+    });
+    if (this.data.wallets_password.length == 6) {//密码长度6位时，自动跳转修改支付密码
+      wallet_pay(this)
+    }
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
+  set_wallets_password1(e) {//获取修改支付密码
+    this.setData({
+      wallets_password1: e.detail.value
+    });
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
+  set_Focus() {//聚焦input
+    console.log('isFocus', this.data.isFocus)
+    this.setData({
+      isFocus: true
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
+  set_notFocus() {//失去焦点
+    this.setData({
+      isFocus: false
+    })
   }
 })
+/*-----------------------------------------------*/
+// 验证支付密码
+function wallet_pay(_this) {
+  console.log('验证支付密码')
+  _this.setData({
+    wallets_password_flag: true,
+    isFocus: true
+  })
+}
